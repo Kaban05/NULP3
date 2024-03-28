9f231bd04c536942f2cae3aa335c540140d24845
YuraB <yurii.bilynskyi.ap.2023@lpnu.ua>
2024-03-28 21:17:30 +0200

    All codes

--- NULP3.c
+++ NULP3.c
@@ -8,3 +8,69 @@ int main() {
 	z = y + x;
 	w = y - x;
 }
+
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+main()
+
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int a = 5;
+	float b = 23.5;
+	int c = 31000;
+	printf("%d метрів тканини коштувало %f гривень.\n", a, b);
+	printf("Значення числа рі рівне %f.\n", PI);
+	printf("IBM розповсюдження.\n)");
+	printf("%c%d\n", '$', c);
+
+}
+
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+main()
+{
+	printf("%f\n", 6543.21);
+	printf("%e\n", 6543.21);
+	printf("%4.2f\n", 6543.21);
+	printf("%3.1f\n", 6543.21);
+	printf("%10.3f\n", 6543.21);
+	printf("%10.3e\n", 6543.21);
+}
+
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+#define riadok "Чудова погода"
+
+main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	printf("/%2s/\n", riadok);
+	printf("/%15.s/\n", riadok);
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int vik;
+	char name[30];
+
+	printf("Введіть ваш вік: ");
+	scanf("%d", &vik);
+
+	printf("Введіть ваше ім'я: ");
+	scanf("%s", name);
+
+	printf("Привіт, %s, якому(ій) %d років!\n", name, vik);
+
+	return 0;
+}
