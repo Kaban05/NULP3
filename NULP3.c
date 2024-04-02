69390fc19ee324732423d2eec3ab1805e8cf0d76
YuraB <yurii.bilynskyi.ap.2023@lpnu.ua>
2024-04-02 10:14:08 +0300

    Add File 6labu ta 6a

--- NULP3.c
+++ NULP3.c
@@ -1,27 +1,82 @@
+п»ї//Laboratorna #6
+
+//Zavdannya 1
 #include <stdio.h>
-#include <windows.h>
 
 int main() {
+    int z;
+    int w;
+    int x = 1;
+    int y = 2;
+    z = y + x;
+    w = y - x;
+}
+
+//Zavdannya 2
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int a = 5;
+	float b = 23.5;
+	int c = 31000;
+	printf("%d РјРµС‚СЂС–РІ С‚РєР°РЅРёРЅРё РєРѕС€С‚СѓРІР°Р»Рѕ %f РіСЂРёРІРµРЅСЊ.\n", a, b);
+	printf("Р—РЅР°С‡РµРЅРЅСЏ С‡РёСЃР»Р° СЂС– СЂС–РІРЅРµ %f.\n", PI);
+	printf("IBM СЂРѕР·РїРѕРІСЃСЋРґР¶РµРЅРЅСЏ.\n)");
+	printf("%c%d\n", '$', c);
+
+}
+
+//Zavdannya 3
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+main() {
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
-    int  i = 10;
-    float f = 3.14;
-    char ch = 'A';
-    long LN = 1234567890;
+    printf("%f\n", 6543.21);
+    printf("%e\n", 6543.21);
+    printf("%4.2f\n", 6543.21);
+    printf("%3.1f\n", 6543.21);
+    printf("%10.3f\n", 6543.21);
+    printf("%10.3e\n", 6543.21);
+}
 
-    printf("Без модифікаторів:\n");
-    printf("Ціле число: %d\n", i);
-    printf("Дійсне число: %f\n", f);
-    printf("Символ: %c\n", ch);
-    printf("Довге ціле число: %ld\n", LN);
-   
-    printf("    \n");
-    
-    printf("Використання модифікаторів:\n");
-    printf("Ціле число (знак): %+d\n", i); // Показує знак
-    printf("Дійсне число (відображення): %e\n", f); // Відображення у форматі e
-    printf("Символ (як ціле число): %d\n", ch); // Виведе ASCII-код символу
-    printf("Довге ціле число (шістнадцяткове): %lx\n", LN); // Перетворення у шістнадцяткову форму
+//Zavdannya 4
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+#define riadok "Р§СѓРґРѕРІР° РїРѕРіРѕРґР°"
 
-    return 0;
+main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    printf("/%2s/\n", riadok);
+    printf("/%15.s/\n", riadok);
+}
+
+//Zavdannya 5
+#define _CRT_SECURE_NO_WARNINGS
+#define PI 3.14159
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int vik;
+	char name[30];
+
+	printf("Р’РІРµРґС–С‚СЊ РІР°С€ РІС–Рє: ");
+	scanf("%d", &vik);
+
+	printf("Р’РІРµРґС–С‚СЊ РІР°С€Рµ С–Рј'СЏ: ");
+	scanf("%s", name);
+
+	printf("РџСЂРёРІС–С‚, %s, СЏРєРѕРјСѓ(С–Р№) %d СЂРѕРєС–РІ!\n", name, vik);
+
+	return 0;
 }
